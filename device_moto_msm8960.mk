db3dbbf8e41a3fe496d7b18264d1af9181c3ee20
arrrghhh <arrrghhh@xdandroid.com>
2014-08-11 18:14:27 -0600

    Missed bootani!
    Change-Id: I8d3f5cd83b6a518760f0253bebfab972bcd04b69

--- device_moto_msm8960.mk
+++ device_moto_msm8960.mk
@@ -114,7 +114,7 @@ PRODUCT_COPY_FILES += \
 
 # Include 960x540 boot animation in the zip
 PRODUCT_COPY_FILES += \
-    vendor/cm/prebuilt/common/bootanimation/540.zip:system/media/540.zip
+    vendor/Gummy/prebuilt/common/bootanimation/540.zip:system/media/540.zip
 
 # Alternate optional key maps
 PRODUCT_PACKAGES += \
